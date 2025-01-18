1cf5ffd1c383f9fae8a426d4e3df52ba8771a543
Will Hopkins <willothyh@gmail.com>
2025-01-18 13:15:48 -0800

    joint mapping abstraction, `Arc<Mutex<_>>` wrap services

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,51 +1,79 @@
 use kos::{
-  hal::{
-    actuator_service_client::ActuatorServiceClient,
-    imu_service_client::ImuServiceClient,
-    inference_service_client::InferenceServiceClient,
-    led_matrix_service_client::LedMatrixServiceClient,
-    process_manager_service_client::ProcessManagerServiceClient,
-    sound_service_client::SoundServiceClient,
-  },
-  kos_proto::system::system_service_client::SystemServiceClient,
+    hal::{
+        actuator_service_client::ActuatorServiceClient, imu_service_client::ImuServiceClient,
+        inference_service_client::InferenceServiceClient,
+        led_matrix_service_client::LedMatrixServiceClient,
+        process_manager_service_client::ProcessManagerServiceClient,
+        sound_service_client::SoundServiceClient,
+    },
+    kos_proto::system::system_service_client::SystemServiceClient,
 };
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tonic::transport::Channel;
 
 pub mod proto {
-  pub use kos::google_proto as google;
-  pub use kos::kos_proto::*;
+    pub use kos::google_proto as google;
+    pub use kos::kos_proto::*;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+pub enum Joint {
+    LeftShoulder,
+    LeftElbow,
+    LeftGripper,
+
+    RightShoulder,
+    RightElbow,
+    RightGripper,
+
+    LeftHip,
+    LeftKnee,
+    LeftAnkle,
+
+    RightHip,
+    RightKnee,
+    RightAnkle,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+pub enum Axis {
+    Pitch,
+    Yaw,
+    Roll,
+}
+
+pub trait JointMapping {
+    fn get_actuator_id(joint: Joint, axis: Axis) -> Option<u32>;
 }
 
 #[derive(Debug, Clone)]
 pub struct Client {
-    pub imu: proto::imu::imu_service_client::ImuServiceClient<Channel>,
-    pub actuator: proto::actuator::actuator_service_client::ActuatorServiceClient<Channel>,
-    pub sound: proto::sound::sound_service_client::SoundServiceClient<Channel>,
-    pub processes:
-        proto::process_manager::process_manager_service_client::ProcessManagerServiceClient<
-            Channel,
-        >,
-    pub led_matrix: proto::led_matrix::led_matrix_service_client::LedMatrixServiceClient<Channel>,
-    pub inference: proto::inference::inference_service_client::InferenceServiceClient<Channel>,
-    pub system: proto::system::system_service_client::SystemServiceClient<Channel>,
+    pub imu: Arc<Mutex<ImuServiceClient<Channel>>>,
+    pub actuator: Arc<Mutex<ActuatorServiceClient<Channel>>>,
+    pub sound: Arc<Mutex<SoundServiceClient<Channel>>>,
+    pub processes: Arc<Mutex<ProcessManagerServiceClient<Channel>>>,
+    pub led_matrix: Arc<Mutex<LedMatrixServiceClient<Channel>>>,
+    pub inference: Arc<Mutex<InferenceServiceClient<Channel>>>,
+    pub system: Arc<Mutex<SystemServiceClient<Channel>>>,
 }
 
 pub use proto::actuator::{ActuatorCommand, CommandActuatorsRequest};
 
 impl Client {
-  pub async fn connect(addr: impl Into<String>) -> eyre::Result<Self> {
-    let conn = tonic::transport::Endpoint::new(addr.into())?
-      .connect()
-      .await?;
+    pub async fn connect(addr: impl Into<String>) -> eyre::Result<Self> {
+        let conn = tonic::transport::Endpoint::new(addr.into())?
+            .connect()
+            .await?;
 
-    Ok(Self {
-      imu: ImuServiceClient::new(conn.clone()),
-      actuator: ActuatorServiceClient::new(conn.clone()),
-      sound: SoundServiceClient::new(conn.clone()),
-      processes: ProcessManagerServiceClient::new(conn.clone()),
-      led_matrix: LedMatrixServiceClient::new(conn.clone()),
-      inference: InferenceServiceClient::new(conn.clone()),
-      system: SystemServiceClient::new(conn),
-    })
-  }
+        Ok(Self {
+            imu: Arc::new(Mutex::new(ImuServiceClient::new(conn.clone()))),
+            actuator: Arc::new(Mutex::new(ActuatorServiceClient::new(conn.clone()))),
+            sound: Arc::new(Mutex::new(SoundServiceClient::new(conn.clone()))),
+            processes: Arc::new(Mutex::new(ProcessManagerServiceClient::new(conn.clone()))),
+            led_matrix: Arc::new(Mutex::new(LedMatrixServiceClient::new(conn.clone()))),
+            inference: Arc::new(Mutex::new(InferenceServiceClient::new(conn.clone()))),
+            system: Arc::new(Mutex::new(SystemServiceClient::new(conn))),
+        })
+    }
 }
