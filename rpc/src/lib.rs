01edf4b06fd49cfaf9d39e52ca27c1b25ac07387
Will Hopkins <willothyh@gmail.com>
2025-01-18 13:47:02 -0800

    joint mappings for kbot

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -10,7 +10,7 @@ use kos::{
 };
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use tonic::transport::Channel;
+use tonic::{transport::Channel, Result};
 
 pub mod proto {
     pub use kos::google_proto as google;
@@ -44,7 +44,7 @@ pub enum Axis {
 }
 
 pub trait JointMapping {
-    fn get_actuator_id(joint: Joint, axis: Axis) -> Option<u32>;
+    fn get_actuator_id(joint: Joint, axis: Option<Axis>) -> Option<u32>;
 }
 
 #[derive(Debug, Clone)]
@@ -75,5 +75,51 @@ impl Client {
             inference: Arc::new(Mutex::new(InferenceServiceClient::new(conn.clone()))),
             system: Arc::new(Mutex::new(SystemServiceClient::new(conn))),
         })
+    }
+}
+
+pub struct KBot {
+    client: Client,
+}
+
+impl JointMapping for KBot {
+    fn get_actuator_id(joint: Joint, axis: Option<Axis>) -> Option<u32> {
+        Some(match (joint, axis) {
+            (Joint::LeftShoulder, Some(Axis::Yaw)) => 11,
+            (Joint::LeftShoulder, Some(Axis::Pitch)) => 12,
+            (Joint::LeftElbow, Some(Axis::Yaw)) => 13,
+            (Joint::LeftGripper, None) => 14,
+
+            (Joint::RightShoulder, Some(Axis::Yaw)) => 21,
+            (Joint::RightShoulder, Some(Axis::Pitch)) => 22,
+            (Joint::RightElbow, Some(Axis::Yaw)) => 23,
+            (Joint::RightGripper, None) => 24,
+
+            (Joint::LeftHip, Some(Axis::Yaw)) => 31,
+            (Joint::LeftHip, Some(Axis::Roll)) => 32,
+            (Joint::LeftHip, Some(Axis::Pitch)) => 33,
+            (Joint::LeftKnee, Some(Axis::Pitch)) => 34,
+            (Joint::LeftAnkle, Some(Axis::Pitch)) => 35,
+
+            (Joint::RightHip, Some(Axis::Yaw)) => 41,
+            (Joint::RightHip, Some(Axis::Roll)) => 42,
+            (Joint::RightHip, Some(Axis::Pitch)) => 43,
+            (Joint::RightKnee, Some(Axis::Pitch)) => 44,
+            (Joint::RightAnkle, Some(Axis::Pitch)) => 45,
+
+            _ => return None,
+        })
+    }
+}
+
+impl KBot {
+    pub async fn connect(addr: String) -> eyre::Result<Self> {
+        let client = Client::connect(addr).await?;
+
+        Ok(Self { client })
+    }
+
+    pub async fn set_joint(&self, joint: Joint, axis: Axis, value: f32) -> eyre::Result<()> {
+        Ok(())
     }
 }
