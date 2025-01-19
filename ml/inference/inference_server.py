24bc06bc068d38d5b09d3a86e8a79189c1493930
karpenet <kedarkarpe10@gmail.com>
2025-01-19 15:30:26 -0500

    fix json payloaf format

--- ml/inference/inference_server.py
+++ ml/inference/inference_server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import torch
 from policy_network import PolicyNetwork
 from inference_policy import sanitize_model_weights
+from inference_utils import compute_gravity_vector
 
 app = Flask(__name__)
 
@@ -38,34 +39,49 @@ else:
 
 # Define scalar constants
 OBS_SCALES = {
-    "ang_vel": 1.0,
+    "lin_vel": 2.0,
+    "ang_vel": 0.25,
+    "command": 1.0,
     "dof_pos": 1.0,
-    "dof_vel": 1.0
+    "dof_vel": 0.05,
 }
-COMMANDS_SCALE = 1.0
-DEFAULT_DOF_POS = torch.zeros(12)
+
+ACTION_SCALE = 0.25
+
+DEFAULT_DOF_POS = torch.zeros(10) 
 OUTPUT_FIELDS = [
-    "LeftShoulder", "LeftElbow", "LeftGripper",
-    "RightShoulder", "RightElbow", "RightGripper",
-    "LeftHip", "LeftKnee", "LeftAnkle",
-    "RightHip", "RightKnee", "RightAnkle"
+    "R_Hip_Pitch",
+    "L_Hip_Pitch",
+    "R_Hip_Yaw",
+    "L_Hip_Yaw",
+    "R_Hip_Roll",
+    "L_Hip_Roll",
+    "R_Knee_Pitch",
+    "L_Knee_Pitch",
+    "R_Ankle_Pitch",
+    "L_Ankle_Pitch",
 ]
 
 @app.route('/infer', methods=['POST'])
 def infer():
     data = request.json
 
+    accel_vector = (data['ax'], data['ay'], data['az'])
+    projected_gravity = compute_gravity_vector(*accel_vector)
+
     input_data = torch.cat([
         torch.tensor(data['base_ang_vel']) * OBS_SCALES['ang_vel'],
-        torch.tensor(data['projected_gravity']),
-        torch.tensor(data['commands']) * COMMANDS_SCALE,
+        torch.tensor(projected_gravity),
+        torch.tensor(data['commands']) * OBS_SCALES['command'],
         (torch.tensor(data['dof_pos']) - DEFAULT_DOF_POS) * OBS_SCALES['dof_pos'],
         torch.tensor(data['dof_vel']) * OBS_SCALES['dof_vel'],
         torch.tensor(data['actions'])
     ], axis=-1).float().to(device)
 
+    print(input_data.shape)
+
     with torch.inference_mode():
-        output = compiled_model(input_data.unsqueeze(0))
+        output = compiled_model(input_data.unsqueeze(0)) * ACTION_SCALE
 
     output_data = {field: value for field, value in zip(OUTPUT_FIELDS, output.squeeze(0).tolist())}
     return jsonify(output_data)
